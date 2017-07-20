615d8305e2f5a2151a01190c2bc8c62b93e3b48d
CaberoDaniel <Daniel.Cabero@fundacion-jala.org>
2017-07-20 11:55:09 +0000

    cucumber task

---
+++ DanielCabero/features/step_definitions/mysteps.rb
@@ -0,0 +1,26 @@
+
+# this method is the given of the test of the cucumber.
+Given(/^generate two numbers\.$/) do
+
+  @num1=rand 100
+  @num2=rand 100
+  puts " fisrt is #{@num1} the second is #{@num2}"
+end
+
+# the action method is sum two numbers.
+When(/^sum the two numbers\.$/) do
+   @result=@num1+@num2
+end
+
+# the result of the Action method.
+Then(/^the result should be display\.$/) do
+  puts " the result is #{@result}"
+end
+# the action method is rest two numbers
+When(/^rest the two numbers\.$/) do
+ @result=@num1-@num2
+end
+
+When(/^times the two numbers\.$/) do
+  @result = @num2*@num1
+end
---
+++ DanielCabero/features/test.feature
@@ -0,0 +1,19 @@
+
+ Feature: only test
+   this is test about the cucumber with ruby.
+   test for the correct write of the feature.
+
+   Scenario: test cucumber sum two numbers
+     Given generate two numbers.
+     When sum the two numbers.
+     Then the result should be display.
+
+   Scenario: test cucumber rest two numbers
+     Given generate two numbers.
+     When rest the two numbers.
+     Then the result should be display.
+
+   Scenario: times of the two numbers
+     Given generate two numbers.
+     When times the two numbers.
+     Then the result should be display.
